c575f116f58206bbc6218373718da74d3c542e1f
Adrian Lam <75461199+adrian-8901@users.noreply.github.com>
2021-05-14 22:21:42 +0800

    Update hal.sh

--- hal.sh
+++ hal.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-alias c="git clone"
 
 cd ../.. 
 echo "Found makefile, cloning"
 rm -rf hardware/qcom-caf/sm8250/display
-c https://github.com/adrian-8901/hardware_qcom-caf_sm8250_display.git -b eleven hardware/qcom-caf/sm8250/display
+git clone https://github.com/adrian-8901/hardware_qcom-caf_sm8250_display.git -b eleven hardware/qcom-caf/sm8250/display
 echo "Finished. Continuing..."
